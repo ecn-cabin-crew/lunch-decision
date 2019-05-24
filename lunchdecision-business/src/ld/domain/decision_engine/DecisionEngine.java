2d1296efc2b1e078830c250910109d86d6f5e96f
Yi Wei <pangyiwei@gmail.com>
2019-05-24 14:43:20 +0800

    Add DecisionEngine Interface

---
+++ lunchdecision-business/src/ld/domain/decision_engine/DecisionEngine.java
@@ -0,0 +1,18 @@
+package ld.domain.decision_engine;
+
+import java.util.List;
+
+import ld.domain.constraint.Constraint;
+import ld.domain.venue.Venue;
+
+public interface DecisionEngine {
+	public Venue[] getRecommendation(int numberOfRecommendations);
+	
+	public void addHardContraint(Constraint constraint);
+	
+	public void addSoftContraint(Constraint constraint);
+	
+	public void resetConstraints();
+	
+	public void setVenuePool(List<Venue> venuePool);
+}
